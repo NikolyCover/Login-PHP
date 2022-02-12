4e6280ea7e8362281f36378cd6427851762b5a92
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-02-12 17:43:45 -0300

    feature: get out button

--- sucess.php
+++ sucess.php
@@ -1,3 +1,13 @@
+<?php
+
+/*session_start();
+
+if(!$_SESSION['user']) {
+    header('Location: ../index.php');
+    exit();
+}*/
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,14 +21,23 @@
             background: url(https://c.tenor.com/zoD-a4zYa-EAAAAC/entrapta-entrapdak.gif) repeat;
             background-size: cover;
         }
+
+        input {
+            background-color: #e62020;
+            border: none;
+            color: #FFF;
+            border-radius: 10px;
+            padding: 10px 40px;
+            position: absolute;
+            bottom: 4vh;
+            right: 4vw;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
+    <form action="scripts/logOut.php">
+        <input type="submit" value="Sair" name="out">
+    </form>
 </body>
 </html>
-
-<?php
-
-include('scripts/checkLogin.php');
-
-?>
