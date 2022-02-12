5eadfca025cc9178394327522bf2c65ca4fc57c8
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-02-12 18:15:36 -0300

    feature: get out button

--- scripts/login.php
+++ scripts/login.php
@@ -19,7 +19,6 @@ $result = mysqli_query($connection, $query);
 $row = mysqli_num_rows($result);
 
 if($row == 1) {
-    $_SESSION['user'] = $email;
     header('Location: ../sucess.php');
     exit();
 } else {
