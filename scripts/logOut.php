0e3c8a19d553fe7a4730e0b976e2eb1da2221fd1
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2022-02-12 17:52:08 -0300

    feature: get out button

--- scripts/logOut.php
+++ scripts/logOut.php
@@ -1,4 +1,6 @@
 <?php
 
-session_destroy($_SESSION['user']);
-header('Location: ../index.php');
+if(!empty($_POST['out'])) {
+    session_destroy($_SESSION['user']);
+    header('Location: ../index.php');
+}
